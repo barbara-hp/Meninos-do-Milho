57cd38c90e79acfae5529d34501f7d38393a3100
barbara-hp <117123820+barbara-hp@users.noreply.github.com>
2023-11-05 19:14:57 -0300

    alteção estrutura

--- modal.js
+++ modal.js
@@ -4,39 +4,81 @@
 const cards = document.querySelectorAll(".card");
 const buttonsOpen = document.querySelectorAll(".btn--show-modal");
 
-const produtosModal = [
-  [
-    { nome: "Pamonha doce", preco: "8" },
-    { nome: "Pamonha doce com queijo", preco: "8" },
-    { nome: "Pamonha com linguiça e queijo", preco: "R$ 8" }
+const produtosModal = {
+  pamonha: [
+    {
+      nome: "Pamonha doce",
+      preco: "8.00"
+    },
+    {
+      nome: "Pamonha doce com queijo",
+      preco: "8.00"
+    },
+    {
+      nome: "Pamonha com linguiça e queijo",
+      preco: "8.00"
+    }
   ],
-  [
-    { nome: " Curau quente", preco: "R$ 8" },
-    { nome: "Curau temperatura ambiente", preco: "8" },
-    { nome: "Curau gelado", preco: "8" }
+  curau: [
+    {
+      nome: "Curau quente",
+      preco: "8.00"
+    },
+    {
+      nome: "Curau temperatura ambiente",
+      preco: "8.00"
+    },
+    {
+      nome: "Curau gelado",
+      preco: "8.00"
+    }
   ],
-  [
-    { nome: "Pedaço de bolo", preco: "8" }
-  ],
-  [
-    { nome: "Suco de milho", preco: "8" }
-  ],
-  [
-    { nome: "Quenga com frango", preco: "8" },
-    { nome: "Quenga com calabresa e bacon", preco: "8" }
-  ],
-  [
-    { nome: "Milho cozido", preco: "8" }
-  ],
-  [
-    { nome: "Geleia de morango", preco: "8" },
-    { nome: "Geleia de morango com amora", preco: "8" },
-    { nome: "Geleia de pimenta", preco: "8" },
-  ],
-  [
-    { nome: "Polpa de milho ralado", preco: "8" }
-  ],
-];
+  bolo: [
+    {
+      nome: "Pedaço de bolo",
+      preco: "8.00"
+    }],
+  suco: [
+    {
+      nome: "Suco de milho",
+      preco: "7.00"
+    }],
+
+  quengas: [{
+    nome: "Quenga com frango",
+    preco: "8.00"
+  },
+  {
+    nome: "Quenga com calabresa e bacon",
+    preco: "8.00"
+  }],
+
+  milhoCozido: [{
+    nome: "Milho cozido",
+    preco: "3.00"
+  }],
+
+  geleias: [
+    {
+      nome: "Geleia de morango",
+      preco: "10.00"
+    },
+    {
+      nome: "Geleia de morango com amora",
+      preco: "10.00"
+    },
+    {
+      nome: "Geleia de pimenta",
+      preco: "10.00"
+    }],
+
+  polpa: [
+    {
+      nome: "Polpa de milho ralado",
+      preco: "30.00"
+    }
+  ]
+};
 
 cards.forEach((card, index) => {
   const modalContainer = document.createElement('div');
@@ -46,39 +88,52 @@ cards.forEach((card, index) => {
       <h2 class="modal__header">Adicione à sua cesta</h2>
       <button class="btn--close-modal">&times;</button>
     </div>
-    <form class="modal__form">
-   
     <div class="subtitulos-form">
         <h2 class="sabor">Produto</h2>
         <h2 class="quantidade">Quantidade</h2>
-      </div>
-       ${produtosModal[index].map(produto =>
-    `<div class="modal-produto-wrap">
-          <label>${produto.nome}</label>
-          <div class="btns-quantidade-container">
-            <button class="btn-quantidade btn-menos">-</button>
-            <input type="text"  id="quantidade-input-${index}" />
-            <button class="btn-quantidade btn-mais">+</button>
-          </div>
-        </div>`
-  ).join('')}
-      
-    <div class="container-valor">
-    <span class="total-modal">Total</span>
-    <span class="linha linha-modal"></span>
-    <span class="valor valor-modal" id="valor-total-${index}">R$ 0.00</span>
-  </div>
-    </form>
-
-    <div class="btns-opt-container">
-      <button class="btn-opt btn__opt-cesta"><img src="./imgs/carrinhodecompras.svg"/>Ver cesta</button>
-      <button class="btn-opt btn__opt-continuar"><img src="./imgs/add_shopping_cart_FILL0_wght400_GRAD0_opsz24.svg"/>Continuar</button>
-      <button class="btn-opt btn__opt-finalizar"><img src="./imgs/carrinhodecomprasFinalizar.svg"/>Finalizar</button>
     </div>
+
+      <form class="modal__form">
+      
+      ${Object.keys(produtosModal).map((categoria) => {
+    if (categoria === card.getAttribute("id")) {
+      return produtosModal[categoria].map((produto, i) => `
+          <div class="modal-produto-wrap">
+            
+              <p>${produto.nome}</p>
+        
+            <div class="btns-quantidade-container">
+              <button class="btn-quantidade btn-menos">-</button>
+              <input type="number" value="0" id="quantidade-input-${i}"/>
+              <button class="btn-quantidade btn-mais">+</button>
+            </div>
+          </div>
+          `
+      ).join('');
+    }
+  }).join('')}
+    
+  <div class="container-valor container-valor-modal">
+  <span class="total-modal">Total</span>
+  <span class="linha linha-modal"></span>
+  <span class="valor valor-modal" id="valor-total-${index}">R$ 0.00</span>
+</div>
+      </form >
+   
+
+  <div class="btns-opt-container">
+    <button class="btn-opt btn__opt-cesta"><img src="./imgs/carrinhodecompras.svg" />Ver cesta</button>
+    <button class="btn-opt btn__opt-continuar"><img src="./imgs/add_shopping_cart_FILL0_wght400_GRAD0_opsz24.svg" />Continuar</button>
+
+   
+    <button class="btn-opt btn__opt-finalizar"><img src="./imgs/carrinhodecomprasFinalizar.svg" />Finalizar</button>
+
   </div>
-  `;
+`;
   card.appendChild(modalContainer);
 });
+
+
 const overlay = document.querySelector(".overlay");
 const modals = document.querySelectorAll(".modal");
 const buttonsClose = document.querySelectorAll(".btn--close-modal");
@@ -95,13 +150,15 @@ function openModal (index) {
   const cardTop = cardPosition.top;
   const cardHeight = cardPosition.height;
 
-  const scrollPosition = cardTop - (windowHeight / 2) + (cardHeight / 2);
+  const scrollPosition = cardTop - (windowHeight / 2) + (cardHeight / 4);
 
   window.scrollBy({
     top: scrollPosition,
     behavior: 'smooth',
   });
   overlay.classList.remove("hidden");
+  document.body.style.overflowY = "hidden";
+
 }
 
 
@@ -122,15 +179,24 @@ buttonsOpen.forEach((button, index) => {
 buttonsClose.forEach((button, index) => {
   button.addEventListener("click", function () {
     closeModal(index);
+
   });
 });
 
 overlay.addEventListener('click', function () {
   const modal = document.querySelector(".modal.active");
+  const lista = document.querySelector(".lista-cesta");
   if (modal) {
     modal.classList.remove("active");
-    overlay.classList.add("hidden");
   }
+
+  if (lista) {
+    lista.classList.add("hidden");
+  }
+
+  overlay.classList.add("hidden");
+  document.body.style.overflowY = "scroll";
+
 });
 
 const btnsDiminuir = document.querySelectorAll(".btn-menos");
@@ -138,14 +204,14 @@ const btnsAumentar = document.querySelectorAll(".btn-mais");
 const valorTotal = document.querySelectorAll(".valor-modal");
 
 function calcValorTotal () {
-  cards.forEach((card, index) => {
+  cards.forEach((card, key) => {
     let total = 0;
 
-    const inputs = card.querySelectorAll("input[type='text']");
-    const produtos = produtosModal[index];
+    const inputs = card.querySelectorAll("input[type='number']");
+    const produtos = produtosModal[card.getAttribute("id")];
 
     inputs.forEach((input, i) => {
-      const quantidade = parseInt(input.value, 10) || 0;
+      const quantidade = parseInt(input.value);
       const preco = parseFloat(produtos[i].preco.replace('R$', '').trim());
 
       if (!isNaN(preco)) {
@@ -159,42 +225,89 @@ function calcValorTotal () {
   });
 }
 
-calcValorTotal();
+const btnsMais = document.querySelectorAll(".btn-mais");
+const btnsMenos = document.querySelectorAll(".btn-menos");
+const inputs = document.querySelectorAll("input[type='number']");
 
-function alterarValor (input, operacao) {
-  if (input && input.tagName === 'INPUT' && input.type === 'text') {
-    let valorAtual = parseInt(input.value, 10) || 0;
-
-    if (operacao === 'aumentar') {
-      valorAtual += 1;
-      if (valorAtual > 10) {
-        alert("Para pedidos maiores, por gentileza entrar em contato diretamente conosco pelo telefone ou WhatsApp");
-        valorAtual = 10;
-      }
-    } else if (operacao === 'diminuir') {
-      valorAtual -= 1;
-      if (valorAtual < 0) {
-        valorAtual = 0;
-      }
-    }
-
-    input.value = valorAtual;
-    calcValorTotal();
-  }
-}
-
-btnsAumentar.forEach(function (btnAumentar) {
-  btnAumentar.addEventListener("click", function (e) {
+btnsMais.forEach((btnMais, index) => {
+  btnMais.addEventListener("click", (e) => {
     e.preventDefault();
-    const input = btnAumentar.previousElementSibling;
-    alterarValor(input, 'aumentar');
+    const input = inputs[index];
+    let valorAtual = parseInt(input.value) || 0;
+    if (input) {
+      valorAtual++;
+      input.value = valorAtual;
+      calcValorTotal();
+    }
   });
 });
 
-btnsDiminuir.forEach(function (btnDiminuir) {
-  btnDiminuir.addEventListener("click", function (e) {
+btnsMenos.forEach((btnMenos, index) => {
+  btnMenos.addEventListener("click", (e) => {
     e.preventDefault();
-    const input = btnDiminuir.nextElementSibling;
-    alterarValor(input, 'diminuir');
+    const input = inputs[index];
+    let valorAtual = input.value;
+    if (valorAtual > 0) {
+      valorAtual--;
+      input.value = valorAtual;
+      calcValorTotal();
+    }
+  });
+});
+
+
+function renderizaCarrinho (index) {
+
+  const modal = modals[index];
+  modal.classList.remove("active");
+
+  let listaDiv = document.querySelector(".lista-cesta");
+
+  if (!listaDiv) {
+    listaDiv = document.createElement("div");
+    listaDiv.classList.add("lista-cesta");
+    listaDiv.innerHTML = /*html */`
+      <h3 class="carrinho__titulo">Seu carrinho</h3>
+      <div class="conteudo"></div>
+      <div class="container-valor container-valor-modal">
+    <span class="total-modal">Total</span>
+    <span class="linha linha-modal"></span>
+    <span class="valor valor-modal" id="valor-total-${index}">R$ 0.00</span>
+    <div class="btns-opt-container">
+    <button class="btn-opt btn__opt-continuar"><img src="./imgs/add_shopping_cart_FILL0_wght400_GRAD0_opsz24.svg" />Continuar</button>
+
+    <button class="btn-opt btn__opt-finalizar"><img src="./imgs/carrinhodecomprasFinalizar.svg" />Finalizar</button>modal.classList.add("active");
+
+
+  </div>
+    `;
+    const body = document.querySelector("body");
+    body.appendChild(listaDiv);
+  } else {
+    const lista = document.querySelector(".conteudo");
+    lista.innerHTML = "";
+  }
+
+  cards.forEach((card) => {
+    const lista = document.querySelector(".conteudo");
+    const inputs = card.querySelectorAll("input[type='number']");
+    const produtos = produtosModal[card.getAttribute("id")];
+
+    inputs.forEach((input, inputIndex) => {
+      const quantidade = input.value;
+      if (quantidade > 0) {
+        const listItem = document.createElement("p");
+        listItem.textContent = `${quantidade} - ${produtos[inputIndex].nome}`;
+        lista.appendChild(listItem);
+      }
+    });
+  });
+}
+
+const verCesta = document.querySelectorAll(".btn__opt-cesta");
+
+verCesta.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    renderizaCarrinho(index);
   });
 });
